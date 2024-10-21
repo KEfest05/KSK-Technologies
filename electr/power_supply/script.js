1e9f9528f3f2fde7878eaf067311e6b3201586ee
KEfest05 <kostkin05kirill@gmail.com>
2024-10-21 22:21:03 +0500

    modified

--- electr/power_supply/script.js
+++ electr/power_supply/script.js
@@ -4,8 +4,9 @@ function printLog(isMobileSize) {
     const pcbBlock = document.getElementById("main2")
     if(isMobileSize){
         pcbBlock.innerHTML = '<label for="Up">Вид сверху</label> <img id = "Up" src="img/pcbUP.png" alt=""> <label for="Down">Вид снизу</label> <img id = "Down" src="img/pcbDown.png" alt=""></img>'
+        pcbBlock.innerHTML += '<div class="linkBlock"> <a class="link link1" href="files/pcbUp.pdf" download="pcbUp.pdf">Скачать вид сверху</a> <a class="link link1" href="files/pcbDown.pdf" download="pcbDown.pdf">Скачать вид снизу</a> <a class="link" href="files/PCB_Power_site.dip" download="PCB_Power.dip">Скачать файл платы (Diptarce)</a> </div>'
     } else {
-        pcbBlock.innerHTML = '<div class="LabelBlock"> <label for="Up">Вид сверху</label> <label for="Down">Вид снизу</label> </div> <div class="imgBlock"> <img id = "Up" src="img/pcbUP.png" alt=""> <img id = "Down" src="img/pcbDown.png" alt=""> </div>'
+        pcbBlock.innerHTML = '<div class="LabelBlock"> <label for="Up">Вид сверху</label> <label for="Down">Вид снизу</label> </div> <div class="imgBlock"> <img id = "Up" src="img/pcbUP.png" alt=""> <img id = "Down" src="img/pcbDown.png" alt=""> </div> <div class="linkBlock"> <a class="link" href="files/pcbUp.pdf" download="pcbUp.pdf">Скачать вид сверху</a> <a class="link" href="files/pcbDown.pdf" download="pcbDown.pdf">Скачать вид снизу</a> <a class="link" href="files/PCB_Power_site.dip" download="PCB_Power.dip">Скачать файл платы (Diptarce)</a></div>'
     }
 }
 
