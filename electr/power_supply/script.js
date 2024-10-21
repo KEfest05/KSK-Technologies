f628cd0bd8de86527dfd2bb7fb3aa1f4c74ffec2
KEfest05 <kostkin05kirill@gmail.com>
2024-10-21 20:17:20 +0500

    modified

--- electr/power_supply/script.js
+++ electr/power_supply/script.js
@@ -4,10 +4,9 @@ function printLog(isMobileSize) {
     const pcbBlock = document.getElementById("main2")
     if(isMobileSize){
         pcbBlock.innerHTML = '<label for="Up">Вид сверху</label> <img id = "Up" src="img/pcbUP.png" alt=""> <label for="Down">Вид снизу</label> <img id = "Down" src="img/pcbDown.png" alt=""></img>'
-    } 
-    // else {
-    //     pcbBlock.innerHTML = '<div class="LabelBlock"> <label for="Up">Вид сверху</label> <label for="Down">Вид снизу</label> </div> <div class="imgBlock"> <img id = "Up" src="img/pcbUP.png" alt=""> <img id = "Down" src="img/pcbDown.png" alt=""> </div>'
-    // }
+    } else {
+        pcbBlock.innerHTML = '<div class="LabelBlock"> <label for="Up">Вид сверху</label> <label for="Down">Вид снизу</label> </div> <div class="imgBlock"> <img id = "Up" src="img/pcbUP.png" alt=""> <img id = "Down" src="img/pcbDown.png" alt=""> </div>'
+    }
 }
 
 printLog(mobileWidthMediaQuery.matches)
